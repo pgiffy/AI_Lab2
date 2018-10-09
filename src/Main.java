de02f8c5b3533e536581fbbdf0e2bfaac7fd6950
StylishKhakis <peter.lee.gifford@gmail.com>
2018-10-09 12:04:42 -0600

    dramatic changes

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	public boolean backtrack(char[][] grid, int sol){
+		//if there are 0 spots left then return
+		
+		
+		
+		//there is no solution
+		return false;
+	}
+	
 
 	public static void main(String[] args) {
 		/*Notes Section:
@@ -39,18 +49,18 @@ public class Main {
 						xLength = scan.nextLine().length();
 						yLength++;
 					}
-
+					int size = yLength*xLength;
 					int idIter = 0;
 					int perLineIter = 0;
 					int lineIter = 0;
 
 					scan = new Scanner(gridFile);//reseting scanner to the top
-					Node[][] totalMaze = new Node[yLength][xLength];
+					char[][] totalMaze = new char[yLength][xLength];
 					while(scan.hasNext()) {
 						String currentLine = scan.nextLine();
 						perLineIter = 0;
 						for(char con : currentLine.toCharArray()) {
-							totalMaze[lineIter][perLineIter] = new Node(idIter, con, perLineIter, lineIter);
+							totalMaze[lineIter][perLineIter] = con;
 							idIter++;
 							perLineIter++;
 						}
@@ -58,42 +68,12 @@ public class Main {
 						//totalMaze has nodes of all 
 						
 					}
-					Gird grid = new Gird();
-					for(int y = 0; y < yLength; y++) {
-						for(int x = 0; x < xLength; x++) {
-							Node current = totalMaze[y][x];
-							try {
-								//checking all cardinal directions since cant move diagonal
-								if((totalMaze[y+1][x].content == '_' || (totalMaze[y+1][x].content >= 'A' && totalMaze[y+1][x].content <= 'Z')) && totalMaze[y+1][x] != null) {
-									current.friends.add(totalMaze[y+1][x]);
-									grid.addNode(totalMaze[y+1][x]);
-								}
-								if((totalMaze[y][x+1].content == '_' || (totalMaze[y][x+1].content >= 'A' && totalMaze[y][x+1].content <= 'Z')) && totalMaze[y][x+1] != null) {
-									current.friends.add(totalMaze[y][x-1]);
-									grid.addNode(totalMaze[y][x+1]);
-								}
-								if((totalMaze[y-1][x].content == '_' || (totalMaze[y-1][x].content >= 'A' && totalMaze[y-1][x].content <= 'Z')) && totalMaze[y-1][x] != null) {
-									current.friends.add(totalMaze[y-1][x]);
-									grid.addNode(totalMaze[y-1][x]);
-								}
-								if((totalMaze[y][x-1].content == '_' || (totalMaze[y][x-1].content >= 'A' && totalMaze[y][x-1].content <= 'Z')) && totalMaze[y][x-1] != null) {
-									current.friends.add(totalMaze[y][x-1]);
-									grid.addNode(totalMaze[y][x-1]);
-								}
-							}catch(ArrayIndexOutOfBoundsException e) {
+					
+					
 
-							}
-						}
-
-					}
-					
-					
-					//algorithm here
-					
-					
 					for(int i = 0; i < yLength; i++) {
 						for(int j = 0; j < xLength; j++) {
-							out.print(totalMaze[i][j].content);
+							out.print(totalMaze[i][j]);
 						}
 						out.println();
 					}
